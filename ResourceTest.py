00c6dba656db07b8f2ab00a0ed567d0d7e3972bd
chois lu <chois_lu@outlook.com>
2017-06-16 22:52:34 +0800

    添加ResourceTest, yield_test 改动更新

--- ResourceTest.py
+++ ResourceTest.py
@@ -1,17 +1,22 @@
 # -*- coding: utf-8 -*-
 
 from simpy import Resource, Environment, Container
+from queue import PriorityQueue
 
+
+# simulation global
+SIMULATION_SYS_TIME = 70
+# =================================================
 # gas station global params
 GAS_STATION_ALARM_LEVEL = 100  # 加油站预警油位
 GAS_STATION_CAPACITY = 1000  # 加油站总计油量
 GAS_RESOURCE_DISPENSER_CAPACITY = 2  # 加油站加油点数
-GAS_TANKER_MONITOR_TIME = 15  # 加油站预警油位扫描间隔
-GAS_TANKER_ARRIVE_USE_TIME = 10  # 加油车到达耗时
-
+GAS_TANKER_MONITOR_TIME = 15  # 加油站预警油位扫描间隔15 Minutes
+GAS_TANKER_ARRIVE_USING_TIME = 10  # 加油车到达耗时10 Minutes
 # =================================================
-# monitor global params
-
+# car global params
+CAR_ARRIVING_GAP = 5  # 汽车抵达间隔5Minutes
+CAR_REFUELING_TIME = 5  # 汽车加油耗时5Minutes
 # =================================================
 
 
@@ -33,26 +38,37 @@ class GasStation:
 
     def tanker(self, env_station):
         """
-        加油车进程单元： 10s后加油车到达，触发加油事件
+
         """
-        yield env_station.timeout(GAS_TANKER_ARRIVE_USE_TIME)  # Need 10 Minutes to arrive
+        yield env_station.timeout(GAS_TANKER_ARRIVE_USING_TIME)  # Need 10 Minutes to arrive
         print('Tanker arriving at %s' % env_station.now)
         amount = self.gas_tank.capacity - self.gas_tank.level
         yield self.gas_tank.put(amount)
         print('Tanker leaving at %s' % env_station.now)
 
     def car_generator(self, env_car):
-        for i in range(10):
+        """
+        car 机器队列模块
+        :param env_car:
+        :return:
+        """
+        for i in range(2):
             env_car.process(self.car(i, env_car))
-            yield env_car.timeout(5)
+            yield env_car.timeout(CAR_ARRIVING_GAP)  # 每CAR_ARRIVING_GAP秒产生一个car
 
     def car(self, name, env_car):
+        """
+        car 加油站加油机器模块
+        :param name:
+        :param env_car:
+        :return:
+        """
         print('Car %s arriving at %s' % (name, env_car.now))
         with self.fuel_dispensers.request() as req:
             yield req
             print('Car %s starts refueling at %s' % (name, env_car.now))
             yield self.gas_tank.get(40)
-            yield env_car.timeout(5)
+            yield env_car.timeout(CAR_REFUELING_TIME)
             print('Car %s done refueling at %s' % (name, env_car.now))
 
 
@@ -60,4 +76,4 @@ if __name__ == '__main__':
     env = Environment()
     gas_station = GasStation(env)
     car_gen = env.process(gas_station.car_generator(env))
-    env.run(70)
+    env.run(SIMULATION_SYS_TIME)
--- yield_test.py
+++ yield_test.py
@@ -1,19 +1,31 @@
 # -*- coding: utf8 -*-
 
-from inspect import isgeneratorfunction
+from simpy import Environment
 
 
-def fab(max):
+def fab(max_f):
     n, a, b = 0, 0, 1
-    while n < max:
+    while n < max_f:
         yield b
         # print b
         a, b = b, a + b
         n = n + 1
 
 
+def gene_simpy(env_gen, num):
+    print('enter ')
+    for i in range(num):
+        env_gen.process(simpy_test(env_gen, i))
+        yield env_gen.timeout(5)
+
+
+def simpy_test(env_sim_test, num):
+    print('sim of %s begin at %s' % (num, env_sim_test.now))
+    yield env_sim_test.timeout(10)
+    print('sim of %s end at %s' % (num, env_sim_test.now))
+
+
 if __name__ == '__main__':
-    f = fab(5)
-    print(type(f))
-    for i in f:
-        print(i)
+    env = Environment()
+    sim_gen = env.process(gene_simpy(env, 5))
+    env.run(50)
